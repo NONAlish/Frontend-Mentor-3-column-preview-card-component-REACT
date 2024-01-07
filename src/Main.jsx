4722d7b2302e0b34c70a0214342f9f78d316efda
Alisher <alisherwashere@gmail.com>
2024-01-07 18:15:18 +0600

    first commit

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,7 +2,7 @@ import "./Main.css"
 import React, { useState } from "react";
 
 
-export default function Main({main_text, text, button, backgroundColor}){
+export default function Main({main_text, text, button, backgroundColor, style_btn, svg}){
     const mainStyle = {
         backgroundColor: backgroundColor,
       };
@@ -11,13 +11,19 @@ export default function Main({main_text, text, button, backgroundColor}){
     return (
         <main style={mainStyle}>
             <li>
+                <img className="svg" src={svg} alt="svg" />
                 <h1>{main_text}</h1>
                 <p>{text}</p>
                 <button 
-                    className={isHovered ? "hovered-button" : "normal-button"}
-                    onMouseEnter={()=>setIsHovered(true)}
-                    onMouseLeave={()=> setIsHovered(false)}
-                >{button}</button>
+                    className={`${
+                        isHovered ? style_btn : "normal-button"
+                    }`}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                    >
+                    {button}
+                </button>
+
             </li>
         </main>
     )
